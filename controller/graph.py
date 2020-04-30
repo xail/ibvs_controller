9dea71492c4c6819ba6dca0a773991f9d02bc03e
Xail <xail@yandex.ru>
2020-04-30 14:37:37 +0300

    added plotVel

--- controller/graph.py
+++ controller/graph.py
@@ -4,31 +4,18 @@ from os.path import expanduser
 home = expanduser("~")
 
 
-def plotVel_lin(v, t, name='v', folder=home + '/resources/', eps=True):
+def plotVel(v, t, name='v', folder=home + '/resources/', eps=True, linear=True):
     if len(v) > 0:
         if len(v) != len(t):
             t = t[0, len(v)]
         plt.plot(t, v, color='black')
         plt.gca().set_xlabel('t, с')
-        plt.gca().set_ylabel('v, м/с')
+        if linear:
+            plt.gca().set_ylabel('v, м/с')
+        else:
+            plt.gca().set_ylabel('$/omega$, рад/с')
         plt.gca().set_xlim(left=min(t), right=max(t))
         plt.gca().set_ylim(bottom=min(v), top=max(v))
-        if eps:
-            plt.savefig(folder + name + '.eps')
-        else:
-            plt.savefig(folder + name + '.pdf')
-        plt.close()
-
-
-def plotVel_ang(w, t, name='omega', folder=home + '/resources/', eps=True):
-    if len(w) > 0:
-        if len(w) != len(t):
-            t = t[0, len(w)]
-        plt.plot(t, w, color='black')
-        plt.gca().set_xlabel('t, с')
-        plt.gca().set_ylabel('omega, рад/с')
-        plt.gca().set_xlim(left=min(t), right=max(t))
-        plt.gca().set_ylim(bottom=min(w), top=max(w))
         if eps:
             plt.savefig(folder + name + '.eps')
         else:
