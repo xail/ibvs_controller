220e6032f708312dc23a96d60bd0f0479efc0667
Alex Utkin <32414050+xail@users.noreply.github.com>
2020-08-28 16:02:30 +0300

    Update features_extr.py

--- controller/features_extr.py
+++ controller/features_extr.py
@@ -3,11 +3,9 @@ import cv2 as cv
 
 Threshold = 200
 
-
+# legacy version of feature extraction
 def extract_f_vec(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    #cuda_im = cv.cuda_GpuMat(cv.cuda.GpuMat_defaultAllocator())
-    #cuda_im.upload(gray)
     detector = cv.AKAZE_create()
     kp, des = detector.detectAndCompute(gray, None)
     s = np.ndarray(shape=(1, 2), dtype=float)
@@ -15,7 +13,7 @@ def extract_f_vec(img):
         s = np.append(s, [[point.pt[0], point.pt[1]]], axis=0)
     return s
 
-
+# function for converting keypoints to s feature vector
 def kp_to_s(kp, x=0, y=0):
     s = np.ndarray(shape=(1, 2), dtype=float)
     for point in kp:
